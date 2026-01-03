215857774a67cee069c1653f3db78863377d336f
VundaRoy <roychang@live.com.au>
2026-01-03 20:36:32 +1100

    Change loop start year from 1999 to 1970

--- tcpl/Chapter2/LeapYear.c
+++ tcpl/Chapter2/LeapYear.c
@@ -3,7 +3,7 @@
 int main() {
   //loop through years
   int year = 0;
-  for( year=1999; year < 2029; ++year)
+  for( year=1970; year < 2029; ++year)
   {
     if((year % 4 == 0 && year  % 100 != 0 ) || year % 400 == 0)
       printf("%d is a leap year \n", year);

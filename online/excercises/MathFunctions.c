cab2520200528fa219c6f32af269f5c4e55bb79e
VundaRoy <roychang@live.com.au>
2026-01-24 20:31:16 +1100

    Add MathFunctions.c to demonstrate math functions

---
+++ online/excercises/MathFunctions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("sqrt(16) %f\n", sqrt(16));
+  printf("ceil(1.4) %f\n", ceil(1.4));
+  printf("floor(25.5) %f\n", floor(25.5));
+  printf("floor(25.99) %f\n", floor(25.99));
+  printf("floor(25.2) %f\n", floor(25.2));
+  printf("pow(4,3) %f", pow(4, 3));
+  return 0;
+}

837fc378e3db5390cb0e44c54e9bcdf55d5bda75
VundaRoy <roychang@live.com.au>
2026-02-05 11:19:52 +1100

    Add bitcount function to count zero bits
    Implement a function to count all zero bits in an unsigned integer and test it with various values.

---
+++ tcpl/Chapter2/bitCountAllZeroes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int bitcount(unsigned x)
+{
+  int b = 0;
+    // Calculate total bits (usually 32)
+    int iterations = sizeof(unsigned) * 8; 
+    
+    for (int i = 0; i < iterations; i++) {
+        if (!(x & 01)) // Check if the bit is NOT 1
+            b++;
+        x >>= 1;
+    }
+    return b;   
+}
+int main() {
+  printf("Bitcount(%d) is %d\n",80000000, bitcount(80000000));
+  printf("Bitcount(%d) is %d\n",800000, bitcount(800000));
+  printf("Bitcount(%d) is %d\n",800, bitcount(800));
+  printf("Bitcount(%d) is %d\n",8, bitcount(8));
+  printf("Bitcount(%d) is %d\n",1, bitcount(1));
+  printf("Bitcount(%d) is %d\n",0, bitcount(0));
+  
+  
+  return 0;
+}

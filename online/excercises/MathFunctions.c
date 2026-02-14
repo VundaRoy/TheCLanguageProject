4c88b1a9cd308e49238b8ed93dcac1ee3f4f9413
VundaRoy <roychang@live.com.au>
2026-02-14 22:32:12 +1100

    Enhance MathFunctions with rounding and ceiling examples
    Added additional examples for rounding and ceiling functions.

--- online/excercises/MathFunctions.c
+++ online/excercises/MathFunctions.c
@@ -3,9 +3,15 @@
 
 int main() {
   printf("sqrt(16) %f\n", sqrt(16));
+  printf("roundf(1.4) %f\n", roundf(1.4));
+  printf("roundf(1.49999999) %f\n", roundf(1.49999999));
+  printf("roundf(1.49) %f\n", roundf(1.49));
+  printf("roundf(1.8) %f\n", roundf(1.8));
   printf("ceil(1.4) %f\n", ceil(1.4));
+  printf("ceil(1.0000011) %f\n", ceil(1.0000011));
   printf("floor(25.5) %f\n", floor(25.5));
   printf("floor(25.99) %f\n", floor(25.99));
+  printf("floor(25.9999999) %f\n", floor(25.9999999));
   printf("floor(25.2) %f\n", floor(25.2));
   printf("pow(4,3) %f", pow(4, 3));
   return 0;

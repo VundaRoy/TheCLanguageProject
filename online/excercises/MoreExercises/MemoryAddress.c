cb2a9efaceb1914765b234371b80d1626c9873ec
VundaRoy <roychang@live.com.au>
2026-03-04 09:52:59 +1100

    Add MemoryAddress.c to display variable addresses
    Prints memory addresses of integer variables.

---
+++ online/excercises/MoreExercises/MemoryAddress.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int myAge = 43;
+  int hisAge = 43;
+  int herAge = 60;
+  int theSize = 109526;
+  printf("myAge %p\n", &myAge);
+  printf("hisAge %p\n", &hisAge);
+  printf("herAge %p\n", &herAge);
+  printf("theSize %p\n", &theSize);
+  return 0;
+}

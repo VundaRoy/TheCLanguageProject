1ef72ffd212cf7fc5e370c933a0016c32c7475e3
VundaRoy <roychang@live.com.au>
2026-04-01 09:10:47 +1100

    Modify loop range and add more bitcount tests
    Updated the range of the loop in main and added additional bitcount tests.

--- tcpl/Chapter2/bitCount.c
+++ tcpl/Chapter2/bitCount.c
@@ -8,15 +8,20 @@ int bitcount(unsigned x)
    return b;     
 }
 int main() {
-  for(int i=92; i<99; i++)
+  for(int i=92; i<95; i++)
   {
     printf("Bitcount(%d) is %d\n",i, bitcount(99));
   }
   
+  printf("Bitcount(%d) is %d\n",8000000000, bitcount(8000000000));
   printf("Bitcount(%d) is %d\n",80000000, bitcount(80000000));
   printf("Bitcount(%d) is %d\n",800000, bitcount(800000));
   printf("Bitcount(%d) is %d\n",800, bitcount(800));
   printf("Bitcount(%d) is %d\n",8, bitcount(8));
+  printf("Bitcount(%d) is %d\n",7, bitcount(7));
+  printf("Bitcount(%d) is %d\n",6, bitcount(6));
+  printf("Bitcount(%d) is %d\n",3, bitcount(3));
+  printf("Bitcount(%d) is %d\n",2, bitcount(2));
   printf("Bitcount(%d) is %d\n",1, bitcount(1));
   printf("Bitcount(%d) is %d\n",0, bitcount(0));
   return 0;

59272d833513fadd523a379baa857598b62a6617
VundaRoy <roychang@live.com.au>
2026-04-24 11:03:07 +1000

    Add test cases for negative numbers in bitcount

--- tcpl/Chapter2/bitCountAllZeroes.c
+++ tcpl/Chapter2/bitCountAllZeroes.c
@@ -19,6 +19,12 @@ int main() {
   printf("Bitcount(%d) is %d\n",8, bitcount(8));
   printf("Bitcount(%d) is %d\n",1, bitcount(1));
   printf("Bitcount(%d) is %d\n",0, bitcount(0));
+  printf("Bitcount(%d) is %d\n",-1, bitcount(-1));
+  printf("Bitcount(%d) is %d\n",-5, bitcount(-5));
+  printf("Bitcount(%d) is %d\n",-100, bitcount(-100));
+  printf("Bitcount(%d) is %d\n",-10000, bitcount(-10000));
+  printf("Bitcount(%d) is %d\n",-10000000, bitcount(-10000000));
+  printf("Bitcount(%d) is %d\n",-40000000, bitcount(-40000000));
   
   
   return 0;

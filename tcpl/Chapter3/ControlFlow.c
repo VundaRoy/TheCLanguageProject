b712456c719cd3ebe323a3ebc26cbc54de45ca8b
VundaRoy <roychang@live.com.au>
2026-05-11 09:25:09 +1000

    Add ControlFlow.c with basic control structures
    Implement control flow with if and for statements.

---
+++ tcpl/Chapter3/ControlFlow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int a = 8;   
+  int b = 5;  
+  int i;
+   if(b > 8)
+       for(i =0; ; i < 4 ; i++)
+         if(b < i)
+          { 
+           printf("%d is less than %d.", b, i);
+          }
+    else
+      printf("error == negative");
+          
+ 
+  return 0;}

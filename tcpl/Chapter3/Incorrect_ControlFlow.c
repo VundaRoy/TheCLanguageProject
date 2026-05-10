ad02f85afc1877415d5317fdafd0cb8c825f994e
VundaRoy <roychang@live.com.au>
2026-05-11 09:34:32 +1000

    Add Incorrect_ControlFlow.c file

--- tcpl/Chapter3/Incorrect_ControlFlow.c
+++ tcpl/Chapter3/Incorrect_ControlFlow.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main() {
+ int main() {
   int a = 8;   
   int b = 5;  
   int i;
    if(b > 8)
-       for(i =0; ; i < 4 ; i++)
+     
+       for(i =0;  i < 4 ; i++)
          if(b < i)
           { 
            printf("%d is less than %d.", b, i);
+          
           }
+     
     else
       printf("error == negative");
-          
- 
-  return 0;}
+  
+  }

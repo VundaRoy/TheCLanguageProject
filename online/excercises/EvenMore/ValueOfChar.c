0b32b53a78230e4b621d4e13c5766a6496c9d3e0
VundaRoy <roychang@live.com.au>
2026-06-10 10:03:42 +1000

    Add ValueOfChar.c to demonstrate char values

---
+++ online/excercises/EvenMore/ValueOfChar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ 
+  char BigA = 'A';
+  char SmallA = 'a';
+  
+  int a1 = BigA; int a2 = SmallA;
+  printf("big A is %d and small a is %d", a1, a2);
+  return 0;
+}

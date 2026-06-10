dc401e75cd1151050a166680215ce5f398212d26
VundaRoy <roychang@live.com.au>
2026-06-10 10:05:02 +1000

    Enhance output to include character representation
    Added a new print statement to display character values.

--- online/excercises/EvenMore/ValueOfChar.c
+++ online/excercises/EvenMore/ValueOfChar.c
@@ -6,6 +6,7 @@ int main() {
   char SmallA = 'a';
   
   int a1 = BigA; int a2 = SmallA;
-  printf("big A is %d and small a is %d", a1, a2);
+  printf("big A is %d and small a is %d\n", a1, a2);
+  printf("big A is %c and small a is %c", BigA, SmallA);
   return 0;
 }
